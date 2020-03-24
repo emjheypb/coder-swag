5bdbf388a822ef7ee09de98e51395a78fb91c238
Mariah Baysic <mjpb123@gmail.com>
2020-03-24 13:40:01 +0800

    created category tableview, category tableviewcell, category model

---
+++ coder-swag/Model/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  coder-swag
+//
+//  Created by Mariah Baysic on 3/24/20.
+//  Copyright © 2020 SpacedOut. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+    private(set) public var title: String
+    private(set) public var image: String
+}
